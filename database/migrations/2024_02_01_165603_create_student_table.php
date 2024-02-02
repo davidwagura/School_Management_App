cff105e91ad1a72023c9080a25e205c50a6bc74d
David wanjohi <davidwagura83@gmail.com>
2024-02-02 09:49:35 +0300

    laravel files

---
+++ database/migrations/2024_02_01_165603_create_student_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string('first-name');
+            $table->string('last-name');
+            $table->timestamp('gender');
+            $table->string('password');
+            $table->integer('admission-number');
+            $table->string('home-county');
+            $table->integer('date-of-birth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student');
+    }
+};
