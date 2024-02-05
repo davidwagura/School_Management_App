29433f89e7f858700af91d76fd68b7069cee5bdc
David wanjohi <davidwagura83@gmail.com>
2024-02-05 11:36:53 +0300

    delete header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,59 @@
     <title>Document</title>
 </head>
 <body>
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: light gray;
+        background-image: url("../bcg.jpg")
+    }
     
-</body>
+    nav {
+        background-color: #151515;
+        padding: 10px;
+        text-align: center;
+    }
+    nav a {
+        color: #fff;
+        text-decoration: none;
+        margin: 0 10px;
+    }
+    .container {
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 20px;
+        text-align: center;
+        padding-top: 10rem;
+        font-size: 2rem;
+    }
+    footer {
+        background-color: #333;
+        color: #fff;
+        padding: 10px 0;
+        text-align: center;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
+</head>
+<body>
+<nav>
+    <a href="/student/create">Students</a>
+    <a href="/county/create">County</a>
+    <a href="/form/create">Forms</a>
+    <a href="/club/create">Clubs</a>
+    <a href="/teacher/create">Teachers</a>
+    <a href="/department/create">Department</a>
+</nav>
+
+<div class="container">
+    <h2>Welcome to our School Management System</h2>
+</div>
+
+<footer>
+    <p>&copy; 2024 School Management System. All rights reserved.</p>
+</footer>
 </html>
