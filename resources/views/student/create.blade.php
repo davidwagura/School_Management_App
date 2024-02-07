a31e36a7be513344998eb48268d980eabd54c971
David wanjohi <davidwagura83@gmail.com>
2024-02-07 17:06:20 +0300

    add background color

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -7,7 +7,7 @@
             Fill the form below:
         </u>
     </h1>
-    <div class="create-inputs  justify-center items-center pt-6 flex flex-wrap">
+    <div class="create-inputs  justify-center items-center pt-6 flex flex-wrap container bg-orange-200">
         <form action="{{ route('student.store') }}" method="post">
             @csrf
 
@@ -18,22 +18,24 @@
                 <label class="text-center justify-around inline mr-2 mt-8" for="last_name">Last Name:</label>
                 <input class="border rounded px-2 py-1 mt-6" type="text" name="last_name"><br>
 
-                <div>
-                    <label class="text-center justify-around mr-2" for="gender">Gender:</label>
-                    <select class="flex w-60 h-8 bg-white border ml-24" id="gender" name="gender">
+                <div class="flex row mt-4">
+                    <label class="text-center justify-around" for="gender">Gender:</label>
+                    <select class="flex w-full h-8 bg-white border ml-9 rounded" id="gender" name="gender">
                         <option  class="block w-full" value="male">Male</option>
                         <option  class="block w-full" value="female">Female</option>
                     </select>
                 </div><br>
-                <label class="text-center justify-around mr-2" for="password">Password:</label>
+
+                <label class="text-center justify-around mr-2 mt-4" for="password">Password:</label>
                 <input class="border rounded px-2 py-1" type="text" name="password"><br>
 
-
-                <label class="text-center justify-around" for="admission_number">Admission Number:</label>
-                <input class="border rounded px-2 py-1" type="number" name="admission_number"><br>
-                <div>
-                    <label class="text-center justify-around mr-2" for="home_county">County:</label>
-                    <select class="flex w-60 h-8 bg-white border ml-24" id="county" name="home_county">
+                <div class="flex row mt-4">
+                    <label class="justify-around" for="admission_number">Admission Number:</label>
+                    <input class="border rounded px-2 py-1 flex" type="number" name="admission_number">
+                </div><br>
+                <div class="flex row mt-4">
+                    <label class="text-center justify-around" for="home_county">County:</label>
+                    <select class="flex w-full h-8 bg-white border ml-9" id="county" name="home_county">
                         <option  class="block w-full" value="Nyeri">Nyeri</option>
                         <option  class="block w-full" value="Murang'a">Murang'a</option>
                         <option  class="block w-full" value="Murang'a">Murang'a</option>
@@ -41,6 +43,7 @@
                         <option  class="block w-full" value="Nakuru">Nakuru</option>
                     </select>
                 </div><br>
+
                 <label class="text-center justify-around mr-2" for="date_of_birth">Date Of Birth:</label>
                 <input class="border rounded px-2 py-1" type="text" name="date_of_birth"><br>
                 <div class="text-center">
